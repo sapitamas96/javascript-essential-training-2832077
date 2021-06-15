dcabd88bf71ec0901cf78994050e76659b899e08
Tamas <sapitamas96@gmail.com>
2021-06-15 18:05:21 +0200

    first look

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,31 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const wallet = {
+  owner: "Tamas",
+  color: "Green",
+  material: "leather",
+  money: {
+    paper: 5000,
+    coin: 1,
+  },
+
+  howMuchMoney: function () {
+    console.log("Money:", this.money.paper + this.money.coin);
+  },
+};
+
+const tabel = {
+  color: "brown",
+  material: "wood",
+  content: {
+    wallet,
+  },
+
+  whatIsOnTable: function () {
+    console.log("On table:", this.content);
+  },
+};
+
+console.log("Money on wallet:", wallet["howMuchMoney"]());
